1171748a9d9a487a2751b2a29d9df90f61d9ea9e
Euagle <gleiscylima@gmail.com>
2023-01-11 19:36:05 -0300

    fluxo de dados

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { 
-    acessarProductId, 
     users,
     products ,
-    acessarProductsName,
-    createPurchase,
-    getAllPurchasesFromUserId, 
+
     purchases
 } from "./database";
 import cors from 'cors'
@@ -22,63 +19,152 @@ app.use(cors())
 app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
 })
-
+//não precisa de validação, basta refatorar para o uso do try/catch
 app.get("/users", (req: Request, res: Response) => {
-res.status(200).send(users)
-})
+    try {
+      res.status(200).send(users);
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
+    }
+  });
+//não precisa de validação, basta refatorar para o uso do try/catch
 app.get("/products", (req: Request, res: Response) => {
-    res.status(200).send( products )
-    })
-
-
-    app.get("/product/search", (req:Request, res: Response)=>{
-        const q= req.query.q as string
-         const productsFilter=products.filter((product)=>product.name.includes(q))
-     res.status(200).send(productsFilter)
-     })
+    try {
+      res.status(200).send(products);
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
+    }
+  });
+  
+//query params deve possuir pelo menos um caractere
+    
+app.get("/product/search", (req: Request, res: Response) => {
+    let productFilter;
+    try {
+      const q = req.query.q as string;
+  
+      if (q.length <= 1) {
+        res.status(400);
+        throw new Error("query params deve possuir pelo menos um caractere");
+      }
+  
+      productFilter = products.filter((product) => {
+        return product.name.toLowerCase().includes(q.toLowerCase());
+      });
+      res.status(200).send(productFilter);
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
+    }
+  });
 
      //Criando novo usuário
-
-     app.post("/users", (req: Request, res: Response )=>{
-        const id = req.body.id
-        const email = req.body.email
-        const password = req.body.password
-    
-    
-    
-        const newUser: TUser ={
-            id,
-            email,
-            password,
-            
-        }
-    users.push(newUser)
-    res.status(201).send("Cadastro realizado com sucesso")
-    })
+//validar o body
+// extra:
+// não deve ser possível criar mais de uma conta com a mesma id
+// não deve ser possível criar mais de uma conta com o mesmo e-mail
+app.post("/users", (req: Request, res: Response) => {
+    try {
+      const id = req.body.id;
+      const email = req.body.email;
+      const password = req.body.password;
+  
+      const findId = users.find((user) => user.id === id);
+  
+      if (findId) {
+        res.status(400);
+        throw new Error("ID indisponivel");
+      }
+  
+      const findEmail = users.find((user) => user.email === email);
+  
+      if (findEmail) {
+        res.status(400);
+        throw new Error("EMAIL indisponivel");
+      }
+  
+      const newUser: TUser = {
+        id,
+        email,
+        password,
+      };
+  
+      users.push(newUser);
+      res.status(201).send("Usuario criado com sucesso 😎");
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
+    }
+  });
 
     //Criando novo produto com o POST
-    app.post("/products", (req: Request, res: Response )=>{
-        const id = req.body.id
-        const name = req.body.name
-        const price = req.body.price
-        const category = req.body.category
-
-    
-    
-    
-        const newProduct: TProduct ={
-            id,
-            name,
-            price,
-            category
-            
-        }
-    products.push(newProduct)
-    res.status(201).send("Produto cadastrado com sucesso")
-    })
+//     validar o body
+// extra:
+// não deve ser possível criar mais de um produto com a mesma id
+app.post("/products", (req: Request, res: Response) => {
+    try {
+      const id = req.body.id;
+      const name = req.body.name;
+      const price = req.body.price;
+      const category = req.body.category;
+  
+      const findId = products.find((product) => product.id === id);
+  
+      if (findId) {
+        res.status(400);
+        throw new Error("ID indisponivel");
+      }
+  
+      const newProduct: TProduct = {
+        id,
+        name,
+        price,
+        category,
+      };
+  
+      products.push(newProduct);
+      res.status(201).send("Produto criado com sucesso 😎");
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
+    }
+  });
 //Compra criada com o POST
-
+// validar o body
+// extra:
+// id do usuário que fez a compra deve existir no array de usuários cadastrados
+// id do produto que foi comprado deve existir no array de produtos cadastrados
+// a quantidade e o total da compra devem estar com o cálculo correto
 app.post("/purchase", (req:Request, res:Response)=>{
+   
+   try {
     const userId= req.body.userId
     const productId=req.body.productId
     const quantity= req.body.quantity
@@ -87,9 +173,59 @@ app.post("/purchase", (req:Request, res:Response)=>{
   const newPurchase: TPurchase ={userId, productId, quantity, totalPrice}
   purchases.push(newPurchase)
   res.status(201).send("Compra realizada com sucesso")
-  
+   
+   } catch (error) {
+    
+   }
   })
-
+  app.post("/purchases", (req: Request, res: Response) => {
+    try {
+      const userId = req.body.userId;
+      const productId = req.body.productId;
+      const quantity = req.body.quantity;
+      const totalPrice = req.body.totalPrice;
+  
+      const findIdUser = purchases.find((purchase) => purchase.userId === userId);
+  
+      if (!findIdUser) {
+        res.status(400);
+        throw new Error("ID do usuario não existe");
+      }
+  
+      const findIdProduct = products.find(
+        (product) => product.id === productId
+      );
+  
+      if (!findIdProduct) {
+        res.status(400);
+        throw new Error("ID do produto não existe");
+      }
+  
+      
+      if (findIdProduct.price * quantity !== totalPrice) {
+          res.status(400)
+          throw new Error("Total incorreto");
+      }
+  
+      const newPurchase: TPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice,
+      };
+  
+      purchases.push(newPurchase);
+      res.status(201).send("Compra efetuada com sucesso 😎");
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
+    }
+  });
   //exercicio1
 //produtos procurados por id
   app.get("/products/:id", (req: Request, res: Response)=>{
